5d888145c84f2d92e21757d347d02c91d88e05c5
Dragos Dumitrache <dragosd2000@gmail.com>
2020-05-24 19:54:19 +0100

    Git wrappers

--- git_sentry/main/connectivity/git.py
+++ git_sentry/main/connectivity/git.py
@@ -1,10 +1,45 @@
-from github3 import login
+import re
+
+from github3 import login, enterprise_login
 
 from git_sentry.configuration.config_reader import read_config
 
+git_client = None
 
-def connect():
-    git_credentials = read_config()
-    public_github_url, public_github_token = git_credentials[0]
-    git_client = login(token=public_github_token)
+
+class GitClient:
+    def __init__(self):
+        self._git_client = _connect()
+
+    def me(self):
+        return self._git_client.me()
+
+    def search_orgs(self, query):
+        orgs = self._git_client.organizations()
+
+        regex = re.compile(query)
+        matching_orgs = [org for org in orgs if regex.match(org.login)]
+        return matching_orgs
+
+    def search_repos(self, query):
+        orgs, *repos = query.split('/')
+
+        matching_orgs = self.search_orgs(orgs)
+        if repos:
+            repos = repos[0]
+            regex = re.compile(repos)
+            matching_repos = [r for org in matching_orgs for r in org.repositories() if regex.match(r.name)]
+            return matching_repos
+        return []
+
+
+def _connect():
+    global git_client
+    if not git_client:
+        git_credentials = read_config()
+        github_url, github_token = git_credentials[0]
+        if github_url == 'github.com':
+            git_client = login(token=github_token)
+        else:
+            git_client = enterprise_login(url=github_url, token=github_token)
     return git_client
--- git_sentry/main/git_sentry.py
+++ git_sentry/main/git_sentry.py
@@ -3,7 +3,7 @@ import os
 import click
 
 from git_sentry.logging.printer import write, focus_out
-from git_sentry.main.connectivity.git import connect
+from git_sentry.main.connectivity.git import GitClient
 from git_sentry.parsing.toml_parser import parse_toml_configuration
 
 
@@ -15,13 +15,20 @@ def cli():
 @cli.command()
 # @click.argument('toml_path', type=click.Path(exists=True, resolve_path=True))
 # def apply(toml_path):
-def apply():
+@click.argument('query')
+def apply(query):
     toml_path = os.path.expanduser('/test/valid_toml')
 
-    git_client = connect()
+    git_client = GitClient()
     write(f'Hello {focus_out(git_client.me().login)}\n')
 
     org, teams = parse_toml_configuration(toml_path)
+
+    for result in git_client.search_orgs(query):
+        print(result)
+
+    for result in git_client.search_repos(query):
+        print(result)
 
 
 def main():
